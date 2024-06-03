2c048ea408c1b73a39fcda2f5f406c0bc2af2f8b
redaelkate <146549050+redaelkate@users.noreply.github.com>
2024-06-04 00:34:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import re
 
 app = Flask(__name__, static_folder='static')
 def generate_image(prompt):
-    client = openai.OpenAI(api_key="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")
+    client = openai.OpenAI(api_key="")
 
     response = client.images.generate(
         model="dall-e-3",
@@ -19,10 +19,25 @@ def generate_image(prompt):
 
 
 genai.configure(api_key="AIzaSyD3iHjKAdmfMjXwfw8YBA_WAOxMgVzUgM4")
-model1 = genai.GenerativeModel('gemini-1.5-flash-latest',system_instruction=["you are a tutor that teaches illiterate adults numeracy  and you only talk in  arabic (no images)   and in this format \"question?|choice3|choice1|choice2 \" "])
-model2 = genai.GenerativeModel('gemini-1.5-flash-latest',system_instruction=["Design a numeracy course for illiterate adults, conducted solely in Arabic , give just the course with a simple way nohing else , give course by course and don't pass to the next course until i click next. Incorporate emojis to enhance understanding and engagement. Cover topics such as numbers, basic operations, fractions, decimals, percentages, geometry, and measurement."])
+model1 = genai.GenerativeModel('gemini-1.5-flash-latest',system_instruction=["you are a tutor that teaches moroccan illiterate adults basic numeracy you give diverse question about real life situation involving numeracy  and you only talk in  arabic  and the questions don't require any images   and only  in this format \"question?|choice3|choice1|choice2 \" "])
+model2 = genai.GenerativeModel(
+    model_name='gemini-1.5-flash-latest', 
+    system_instruction="""
+    you only generate basic numeracy courses for illiterate adults  in this format:
+     picture of 10 Mail Letters | درس عن الرقم 10:
+
+في هذه الصورة، نرى عشر رسائل بريدية معدة للتسليم في صندوق البريد. يبدو أن هناك الكثير من المراسلات التي يجب معالجتها.
+
+الرسائل: الرقم 10 يعني أن هناك عشر رسائل بريدية في الصورة. يمكن استخدام الرقم 10 لتمثيل العدد الإجمالي للرسائل. يمكن للكبار التفكير في كيفية استخدام البريد السنتي لإرسال الرسائل والتواصل مع الآخرين، وكيفية تنظيم الوقت لمعالجة هذه الرسائل بشكل فعال.
+
+
+
+    the lessons should contain usefull information for adults wanting to learn numeracy in real life situation be at least 14 lines You generate them one by one and when you're told "next", you move to the next lesson, staying in the same format.
+    """
+)
+
 model3 = genai.GenerativeModel('gemini-1.5-flash-latest',system_instruction=["you are a tutor that teaches illiterate adults Phonics and you only talk in  arabic (no images)   and in this format \"question?|choice3|choice1|choice2 \" "])
-model4 = genai.GenerativeModel('gemini-1.5-flash-latest', system_instruction=["Design a reading course for illiterate adults, conducted solely in Arabic. Give just the course in a simple way, nothing else. Give course by course and don't pass to the next course until I click 'Next'. Incorporate emojis to enhance understanding and engagement. Cover topics such as the alphabet, basic vocabulary, sentence structure, reading comprehension, and pronunciation."])
+model4 = genai.GenerativeModel('gemini-1.5-flash-latest', system_instruction=["Design a reading course for illiterate adults, conducted solely in Arabic. Give just the course in a simple way, nothing else. Give course by course and don't pass to the next course until I click 'Next'.Cover topics such as the alphabet, basic vocabulary, sentence structure, reading comprehension, and pronunciation."])
 
 H1=[]
 
@@ -59,7 +74,7 @@ def handwriting():
 @app.route("/getReading")
 def get_reading():
     userText = request.args.get('msg')
-    response = chat4.send_message(userText)
+    response = chat3.send_message(userText)
     plain_text_response = strip_markdown(response.text)
     return plain_text_response
 
@@ -77,7 +92,7 @@ def get_bot_response():
 @app.route("/getquiz")
 def get_bot_reading():
     userText = request.args.get('msg')
-    response = chat4.send_message(userText)
+    response = chat3.send_message(userText)
     return response.text.split("|")
 
 
@@ -85,8 +100,10 @@ def get_bot_reading():
 def get_bot_course():
     userText = request.args.get('msg')
     response = chat2.send_message(userText)
-    plain_text_response = strip_markdown(response.text)
-    return plain_text_response
+    quest=response.text.split("|")
+    quest[0]=strip_markdown(quest[0])
+    quest[1]=strip_markdown(quest[1])
+    return quest
 
 
 @app.route("/generate_image")
